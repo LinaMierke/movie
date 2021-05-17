9a00dc2fbad74bc41c8aafedaf53aa0c0e8310a5
lina mierkE <lina_maria89@hotmail.com>
2021-05-17 15:16:49 -0400

    json data

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,18 @@
+import "./Header.css";
+import React from "react";
+
 export default function Header() {
   return (
     <div>
-      <h2>What to watch</h2>
-      <input type="search" />
-      <select id="type">
-        <option value="Movies">Movies</option>
-        <option value="Series">Series</option>
-        <option value="Episode">Episode</option>
-        <option value="Type" selected>
-          Type
-        </option>
+      <h2>What to Watch</h2>
+      <input type='search' />
+      <select id='Type'>
+        <option value='Movies'>Movies</option>
+        <option value='Series'>Series</option>
+        <option value='Episode'>Episode</option>
+        <option value='Type' select></option>
       </select>
-      <button className="search"> Search</button>
+      <button className='search'>Search</button>
     </div>
   );
 }
