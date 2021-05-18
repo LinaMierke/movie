d9411f3b30d498812f186755543136e3bbc84990
lina mierkE <lina_maria89@hotmail.com>
2021-05-18 15:05:54 -0400

    fetch url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
 import MovieGallery from "./components/MovieGallery";
-import movieData from "./movieData.json";
+// import movieData from "./movieData.json";
 function App() {
-  // const [search, setSearch] = useState([]);
+  const [movieData, setMovieData] = useState([]);
 
-  // useEffect(() => {
-  //   let url = "http://www.omdbapi.com/?i=tt3896198&apikey=8446c93f";
-  //   axios
-  //     .get(url)
-  //     .then((res) => {
-  //       return res;
-  //     })
-  //     .then((data) => {
-  //     setSearch(data);
-  //     });
-  //   // updateReviews();
-  // }, []);
-  // // useEffect(() => {
+  const fetchMovieData = () => {
+    let URL = "http://www.omdbapi.com/?apikey=8446c93f&t=my";
+    fetch(URL)
+      .then((res) => res.json())
+      .then((jsonData) => setMovieData(jsonData));
+  };
+  //prevent the infinity loop and ask for just one time action
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
 
   return (
     <div className="App">
