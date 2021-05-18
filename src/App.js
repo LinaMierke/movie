88ed69329e42b43c03dfe2e5bb162cde6da13233
lina mierkE <lina_maria89@hotmail.com>
2021-05-18 19:42:21 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 import MovieGallery from "./components/MovieGallery";
+import Header from './components/Header'
 // import movieData from "./movieData.json";
 function App() {
   const [movieData, setMovieData] = useState([]);
 
-  const fetchMovieData = () => {
-    let URL = "http://www.omdbapi.com/?apikey=8446c93f&t=my";
+  const fetchMovieData = (searchedValue, selectedTypeValue= null) => {
+    let URL = `http://www.omdbapi.com/?apikey=8446c93f&t=${searchedValue}`;
     fetch(URL)
       .then((res) => res.json())
       .then((jsonData) => setMovieData(jsonData));
   };
   //prevent the infinity loop and ask for just one time action
   useEffect(() => {
-    fetchMovieData();
+    fetchMovieData("my");
   }, []);
 
   return (
     <div className="App">
+      <Header fetchMovieData={fetchMovieData}/>
       <MovieGallery movieData={movieData} />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
-import React from "reac";
+import React from "react";
+import { useState } from "react";
 
-export default function Header() {
+export default function Header(props) {
+  const [searchedValue, setSearchedValue] = useState("");
+  const [selectedTypeValue, setSelectedTypeValue] = useState("");
+
+  const handleInput = (e) => {
+    //prevent the page to refresh everytime i select something
+    e.preventDefault();
+    setSearchedValue(e.target.value);
+  };
+  const handleSelectChange = (e) => {
+    //prevent the page to refresh everytime i select something
+    e.preventDefault();
+    setSelectedTypeValue(e.target.value);
+  };
+  const handleClick = (e) => {
+    e.preventDefault()//here the prop pass the function we created in the App.js
+    props.fetchMovieData(searchedValue, selectedTypeValue)
+  }
   return (
     <div>
       <h1> What to Watch</h1>
       <div>
-        <input type="text" placeholder="Title" />
-        <select id="selection" name="slection" placeholder="Type">
+        <input type="text" placeholder="Title" onChange={handleInput}/>
+        <select id="selection" name="slection" placeholder="Type" onChange={handleSelectChange}>
           <option value="" hidden>
             Type
           </option>
@@ -14,7 +32,7 @@ export default function Header() {
           <option value="series">Series</option>
           <option value="episode">Episode</option>
         </select>
-        <button>Search</button>
+        <button onClick={handleClick}>Search</button>
       </div>
     </div>
   );
